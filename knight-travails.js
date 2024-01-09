67ec67438b3ac4aac391db75a71d5f6f3349166b
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2024-01-09 12:31:49 +0200

    Complete knightMoves function and implement printMoves

--- knight-travails.js
+++ knight-travails.js
@@ -26,6 +26,16 @@ function createGraph() {
 
     const graph = initializeGraph();
 
+    function printPath(bfsInfo, x, y, step = 0) {
+        if (bfsInfo[x][y].distance === 0) {
+            console.log(`You made it in ${step} moves!  Here's your path:`);
+        } else {
+            printPath(bfsInfo, bfsInfo[x][y].predecessor[0], bfsInfo[x][y].predecessor[1], step + 1);
+        }
+
+        console.log(`[${x}, ${y}]`);
+    }
+
     function knightMoves(start, end) {
         const bfsInfo = [];
 
@@ -44,26 +54,34 @@ function createGraph() {
         const queue = [];
         queue.push(start);
 
-        while (queue.length > 0) {
+        outer: while (queue.length > 0) {
             const u = queue.shift();
-            const [uX, uY] = u;
-            for (let x = 0; x < BOARD_SIZE; x++) {
-                for (let y = 0; y < BOARD_SIZE; y++) {
-                    for (let v of graph[x][y]) {
-                        const [newX, newY] = v;
-                        if (bfsInfo[newX][newY].distance === null) {
-                            bfsInfo[newX][newY].distance = bfsInfo[uX][uY].distance + 1;
-                            bfsInfo[newX][newY].predecessor = u;
-                            queue.push(bfsInfo[newX][newY]);
-                        }
-                    }
+            const [x, y] = u;
+            for (let v of graph[x][y]) {
+                const [newX, newY] = v;
+                if (bfsInfo[newX][newY].distance === null) {
+                    bfsInfo[newX][newY].distance = bfsInfo[x][y].distance + 1;
+                    bfsInfo[newX][newY].predecessor = [x, y];
+                    queue.push([newX, newY]);
+                }
+
+                if (newX === end[0] && newY === end[1]) {
+                    break outer;
                 }
             }
-
         }
+
+        printPath(bfsInfo, end[0], end[1]);
     }
 
-        return {
-            knightMoves,
-        }
+    return {
+        knightMoves,
     }
+}
+
+const graph = createGraph();
+console.log('knightMoves([3, 3], [4, 3]):');
+graph.knightMoves([3, 3], [4, 3]);
+
+console.log('knightMoves([0,0],[7,7]):');
+graph.knightMoves([0,0],[7,7]);
